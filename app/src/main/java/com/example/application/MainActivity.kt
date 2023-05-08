40728b399a06bd9af08b8965646c3417c0962e34
nox <dark.tempus@gmail.com>
2023-05-07 20:45:56 -0500

    logo -interf

--- app/src/main/java/com/example/application/MainActivity.kt
+++ app/src/main/java/com/example/application/MainActivity.kt
@@ -19,17 +19,9 @@ class MainActivity : AppCompatActivity() {
     lateinit var  btn_multiplicar:Button
     lateinit var  btn_dividir:Button
     lateinit var  resultado:TextView
-
-
-
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
-
          number_1=findViewById(R.id.edit_text_1)
          number_2=findViewById(R.id.edit_text_2)
          btn_sumar=findViewById(R.id.buttton_suma)
@@ -37,20 +29,14 @@ class MainActivity : AppCompatActivity() {
          btn_multiplicar=findViewById(R.id.button_multiplicacion)
          btn_dividir=findViewById(R.id.button_division)
          resultado=findViewById(R.id.text_view)
-
         fun  sumar  (number1:Double,number2:Double):Double{
             return  number1.toDouble()+number2.toDouble()
         }
-
-
-
-
         btn_sumar.setOnClickListener(View.OnClickListener {
 
             if(number_1.getText().toString().isEmpty()||number_2.getText().toString().isEmpty())
             //EditText contiene algún caracter!
                 resultado.setText("Empty")
-
                     else{
                 val n1:Double=(number_1.text.toString().toDouble())
                 val n2:Double=(number_2.text.toString().toDouble())
@@ -60,88 +46,53 @@ class MainActivity : AppCompatActivity() {
         }
         }
         )
-
-
-
         fun  restar (number1:Double,number2:Double):Double{
             return  number1.toDouble()-number2.toDouble()
         }
         btn_restar.setOnClickListener(View.OnClickListener {
-
             if(number_1.getText().toString().isEmpty()||number_2.getText().toString().isEmpty())
             //EditText contiene algún caracter!
                 resultado.setText("Empty" )
-
             else{
-
                 val n1:Double=(number_1.text.toString().toDouble())
                 val n2:Double=(number_2.text.toString().toDouble())
                 resultado.setText("Total : " + restar(n1,n2))
                 number_1.clearFocus()
                 number_2.clearFocus()
-
             }
-
-
-
-
-
-
         })
         fun  multiplicar (number1:Double,number2:Double):Double{
             return  number1.toDouble()*number2.toDouble()
         }
         btn_multiplicar.setOnClickListener(View.OnClickListener {
-
             if(number_1.getText().toString().isEmpty()||number_2.getText().toString().isEmpty())
             //EditText contiene algún caracter!
                 resultado.setText("Empty")
-
-
             else{
-
-
-
-
                 val n1:Double=(number_1.text.toString().toDouble())
                 val n2:Double=(number_2.text.toString().toDouble())
             resultado.setText("Total : " + multiplicar(n1,n2))
             number_1.clearFocus()
             number_2.clearFocus()
-
-
             }
-
-
         })
         fun  division (number1:Double,number2:Double):Double{
              return  number1.toDouble()/number2.toDouble()
         }
         btn_dividir.setOnClickListener(View.OnClickListener {
-
             if(number_1.getText().toString().isEmpty()||number_2.getText().toString().isEmpty())
             //EditText contiene algún caracter!
                 resultado.setText("Empty")
-
             else{
-
-
             val n1:Double=(number_1.text.toString().toDouble())
             val n2:Double=(number_2.text.toString().toDouble())
     resultado.setText("Total : " + division(n1,n2))
     number_1.clearFocus()
     number_2.clearFocus() }
-
-
             }
         )
        }
-
-
     }
-
-
-
 
 
 
