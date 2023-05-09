3d9e7885469c1ab51f93c1200f1771ca8cac6fc0
nox <dark.tempus@gmail.com>
2023-05-09 11:29:35 -0500

    binding

--- app/src/main/java/com/example/application/MainActivity.kt
+++ app/src/main/java/com/example/application/MainActivity.kt
@@ -7,32 +7,47 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import com.example.application.databinding.ActivityMainBinding
+
 class MainActivity : AppCompatActivity() {
 //varibles
-    lateinit var  number_1:EditText
-    lateinit var  number_2:EditText
-    lateinit var  btn_sumar:Button
-    lateinit var  btn_restar:Button
-    lateinit var  btn_multiplicar:Button
-    lateinit var  btn_dividir:Button
-    lateinit var  resultado:TextView
-    lateinit var  btn_total:Button
+ //  lateinit var  number_1:EditText
+    //lateinit var  number_2:EditText
+    //lateinit var  btn_sumar:Button
+   // lateinit var  btn_restar:Button
+   // lateinit var  btn_multiplicar:Button
+   // lateinit var  btn_dividir:Button
+   // lateinit var  resultado:TextView
+
+    private lateinit var binding:ActivityMainBinding
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        number_1 = findViewById(R.id.edit_text_1)
-        number_2 = findViewById(R.id.edit_text_2)
-        btn_sumar = findViewById(R.id.buttton_suma)
-        btn_restar = findViewById(R.id.button_resta)
-        btn_multiplicar = findViewById(R.id.button_multiplicacion)
-        btn_dividir = findViewById(R.id.button_division)
-        resultado = findViewById(R.id.text_view)
- //       btn_total = findViewById(R.id.button_total)
+
+       super.onCreate(savedInstanceState)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+      //setContentView(R.layout.activity_main)
+
+        var number_1 = binding.editText1
+        var number_2 = binding.editText2
+        var btn_sumar = binding.butttonSuma
+        var btn_restar = binding.buttonResta
+        var btn_multiplicar= binding.buttonMultiplicacion
+        var btn_dividir= binding.buttonDivision
+        var resultado= binding.textView
 
 
 
-
+      //number_1 = findViewById(R.id.edit_text_1)
+      //  number_2 = findViewById(R.id.edit_text_2)
+       // btn_sumar = findViewById(R.id.buttton_suma)
+       // btn_restar = findViewById(R.id.button_resta)
+       // btn_multiplicar = findViewById(R.id.button_multiplicacion)
+       // btn_dividir = findViewById(R.id.button_division)
+       // resultado = findViewById(R.id.text_view)
+       // btn_total = findViewById(R.id.button_total)
 
 
             //FUN SUMAR
@@ -61,24 +76,21 @@ class MainActivity : AppCompatActivity() {
 
 
 //Button sumar
-            btn_sumar.setOnClickListener(View.OnClickListener {
-                if (number_1.getText().toString().isEmpty() || number_2.getText().toString().isEmpty()
-                ) {
+   btn_sumar.setOnClickListener(View.OnClickListener {
+   if (number_1.getText().toString().isEmpty() || number_2.getText().toString().isEmpty()
+               ) {
 // EditText contiene algún caracter!
                     resultado.setText("Empty")
                 } else {
-
                     val n1:Double=(number_1.text.toString().toDouble())
                     val n2:Double=(number_2.text.toString().toDouble())
                     resultado.setText("SUMA:  " + sumar(n1, n2))
                     number_1.clearFocus()
                     number_2.clearFocus()
-
                     val intent = Intent(this, resulActivity::class.java)
                     val op = resultado.text.toString()
                     intent.putExtra("suma", op)
                     startActivity(intent)
-
 
 
 
@@ -93,7 +105,6 @@ class MainActivity : AppCompatActivity() {
                 //EditText contiene algún caracter!
                     resultado.setText("Empty")
                 else {
-
                    val n1:Double=(number_1.text.toString().toDouble())
                      val n2:Double=(number_2.text.toString().toDouble())
                     resultado.setText("RESTA:  " + restar(n1, n2))
@@ -117,8 +128,8 @@ class MainActivity : AppCompatActivity() {
                 //EditText contiene algún caracter!
                     resultado.setText("Empty")
                 else {
-                   val n1:Double=(number_1.text.toString().toDouble())
-                 val n2:Double=(number_2.text.toString().toDouble())
+                val n1:Double=(number_1.text.toString().toDouble())
+                val n2:Double=(number_2.text.toString().toDouble())
                     resultado.setText("MULTIPLICACION:  " + multiplicar(n1, n2))
                     number_1.clearFocus()
                     number_2.clearFocus()
--- app/src/main/java/com/example/application/resulActivity.kt
+++ app/src/main/java/com/example/application/resulActivity.kt
@@ -3,22 +3,49 @@ package com.example.application
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import com.example.application.databinding.ActivityResulBinding
 
 class resulActivity : AppCompatActivity() {
+
+
+    private lateinit var binding:ActivityResulBinding
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
+      //  super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_resul)
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_resul)
+        binding= ActivityResulBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        var   resultadoSuma=findViewById<TextView>((R.id.view_suma))
-        val toltalSuma :String=intent.extras?.getString("suma").orEmpty()
-        resultadoSuma.text="$toltalSuma"
 
-        var   resultadosResta=findViewById<TextView>((R.id.view_resta))
+
+    //    var   resultadoSuma=findViewById<TextView>((R.id.view_suma))
+          var resultadoSuma=binding.viewSuma
+          val totalSuma :String=intent.extras?.getString("suma").orEmpty()
+          resultadoSuma.text="$totalSuma"
+
+     //   var   resultadosResta=findViewById<TextView>((R.id.view_resta))
+        var resultadosResta = binding.viewResta
         val totalResta :String=intent.extras?.getString("resta").orEmpty()
         resultadosResta.text="$totalResta"
 
 
-        var   resultadosMultiplicacion=findViewById<TextView>((R.id.view_multiplicacion))
+   //     var   resultadosMultiplicacion=findViewById<TextView>((R.id.view_multiplicacion))
+        var resultadosMultiplicacion =binding.viewMultiplicacion
         val totalMultiplicacion :String=intent.extras?.getString("multiplicacion").orEmpty()
         resultadosMultiplicacion.text="$totalMultiplicacion"
 
